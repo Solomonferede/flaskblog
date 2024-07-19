b059f37babcdb04b16ab6710a5763004e762aee3
Solomon <solomonf1227@gmail.com>
2024-07-19 09:07:00 +0300

    Initial Flaskblog app

---
+++ flaskblog.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Solomon Ferede',
+        'title': 'Blog Post 1',
+        'content': '''Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. “It's not Latin, though it looks like it, and it actually says nothing,” Before & After magazine answered a curious reader, “Its ‘words’ loosely approximate the frequency with which letters occur in English, which is why at a glance it looks pretty real.”
+
+As Cicero would put it, “Um, not so fast.”
+
+The placeholder text, beginning with the line “Lorem ipsum dolor sit amet, consectetur adipiscing elit”, looks like Latin because in its youth, centuries ago, it was Latin.''',
+        'date_posted': '17 July 2024'
+    },
+    {
+        'author': 'Mekides Anbachew',
+        'title': 'Blog Post 2',
+        'content': '''Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:
+
+“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.”
+The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the layout. A practice not without controversy, laying out pages with meaningless filler text can be very useful when the focus is meant to be on design, not content.''',
+        'date_posted': '18 July 2024'
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', posts=posts, title='About')
+
+if __name__ == '__main__':
+    app.run(debug=True)

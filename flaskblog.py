7e597a5f25c6d08261effa2710188dfba4fe33a1
ubuntu <ubuntu@ubuntu.myguest.virtualbox.org>
2024-07-23 23:04:21 +0300

    flask app

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'e8d8e819b162179bdabd44c318f13054'
 
 posts = [
     {
@@ -32,6 +35,20 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', posts=posts, title='About')
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created sucessfuly for {form.username.data}!', 'Success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('Login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
